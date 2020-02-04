3cd4f2606cc1a22ed3b8b51fc3463fc29bc86f16
PotapenkoS <wrunkiev@ukr.net>
2020-02-04 14:24:16 +0200

    spring-1

---
+++ src/main/java/com/Test.java
@@ -0,0 +1,19 @@
+package com;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Test {
+    @RequestMapping(method = RequestMethod.GET, value = "/hi", produces = "text/plain")
+    public @ResponseBody String test(){
+        return "test";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/test", produces = "text/plain")
+    public @ResponseBody String test2(){
+        return "ashdgsihbfdsbfushdf";
+    }
+}
